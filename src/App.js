047da5b4c74965e00ac8c1d57082fe40f026c469
Michael Abdelshahid <mnabdelshahid@gmail.com>
2020-05-08 19:50:46 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    const projectName = 'survey-form';
+localStorage.setItem('example_project', 'Survey Form');
+    </div>
+  );
+}
+
+export default App;
